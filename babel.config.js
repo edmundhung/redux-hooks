82ec538b96854e68bb6e907cb85edc7c49d4b8da
Edmund Hung <edmund@edstud.io>
2019-12-25 12:43:04 +0100

    Setup build flow

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,8 @@
 // babel.config.js
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env', { targets: { node: 'current' } },
+    presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
+    plugins: [
+        '@babel/plugin-proposal-nullish-coalescing-operator',
+        '@babel/plugin-proposal-optional-chaining',
     ],
-    '@babel/preset-typescript',
-  ],
-  plugins: [
-    "@babel/plugin-proposal-nullish-coalescing-operator",
-    "@babel/plugin-proposal-optional-chaining"
-  ]
 };
---
+++ packages/request/rollup.config.js
@@ -0,0 +1,57 @@
+import babel from 'rollup-plugin-babel';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json';
+
+const extensions = ['.ts'];
+const externalPackages = [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+];
+
+const cjs = {
+    input: './packages/request/src/index.ts',
+    output: {
+        file: './packages/request/lib/index.js',
+        format: 'cjs',
+        indent: false,
+        exports: 'named',
+    },
+    external: externalPackages,
+    plugins: [
+        nodeResolve({
+            extensions,
+        }),
+        typescript({
+            useTsconfigDeclarationDir: true,
+        }),
+        babel({
+            extensions,
+            runtimeHelpers: true,
+        }),
+    ],
+};
+
+const es = {
+    input: './packages/request/src/index.ts',
+    output: {
+        file: './packages/request/es/index.js',
+        format: 'es',
+        indent: false,
+    },
+    external: externalPackages,
+    plugins: [
+        nodeResolve({
+            extensions,
+        }),
+        typescript({
+            tsconfigOverride: { compilerOptions: { declaration: false } },
+        }),
+        babel({
+            extensions,
+            runtimeHelpers: true,
+        }),
+    ],
+};
+
+export default [cjs, es];
